e2b4e9c6ac3c215965250a8d343dd1dd7fdcbb32
Ramon Work <ramoncostapatel@gmail.com>
2025-06-30 15:41:25 -0700

    finalize skeleton (missing transitions)

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,22 +1,30 @@
 import React, { Component } from 'react';
-import './styles/global.css';
+import '../styles/global.css';
+import {Game} from './Game';
 
 type AppState = {
-    gameState: "start" | "playing" | "finished";
+    playState: "start" | "playing" | "finished";
 }
 
 export class App extends Component<{}, AppState> {
     constructor(props: {}) {
         super(props)
         this.state = {
-            gameState: "start"
+            playState: "start"
         }
     }
 
     render() {
-        if (this.state.gameState === "start") {
+        if (this.state.playState === "start") {
             return this.renderStart();
         }
+        else if (this.state.playState === "playing") {
+            return <Game/>
+        }
+        else if (this.state.playState === "finished") {
+            return this.renderFinished();
+        }
+        return <div>Error</div>
     }
 
     renderStart = (): React.ReactElement => {
@@ -24,7 +32,19 @@ export class App extends Component<{}, AppState> {
             <h1>Tic Tac Toe</h1>
             <p>Welcome to the Tic Tac Toe game!</p>
             <p>Enjoy playing!</p>
-            <button className="reset-btn">Start Game</button>
+            <button className="reset-btn" onClick={this.openGame}>Start Game</button>
         </div>
+    }
+
+    renderFinished = (): React.ReactElement => {
+        return <div>
+            <h1>Game Over</h1>
+            <p>Thank you for playing!</p>
+            <button className="reset-btn" onClick={this.openGame}>Play Again</button>
+        </div>
+    }
+
+    openGame = (): void => {
+        this.setState({playState: "playing"})
     }
 }
